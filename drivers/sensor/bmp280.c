b94277987084f3f1b592f2e31b9ba2ab9592178b
Franciszek Trzeciak <franektrz4@gmail.com>
2026-04-19 14:31:07 +0200

    code refactor

--- drivers/sensor/bmp280.c
+++ drivers/sensor/bmp280.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2026 Franciszek Trzeciak
  *
  * SPDX-License-Identifier: Apache-2.0
- * 
+ *
  * This software includes compensation algorithms adapted from the
  * Bosch Sensortec BMP280 datasheet.
  * Original algorithmic logic: Copyright (c) Bosch Sensortec GmbH.
@@ -18,6 +18,7 @@
 
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/spi.h>k.v      
 #include "bmp280_priv.h"
 /*
 chanels
@@ -62,6 +63,24 @@ struct bmp280_calibData
 	bool t_cooef_cmpt;
 };
 
+struct bmp280_ioMethods
+{
+	int (*readByte)(const struct device *dev, uint8_t addr, void *buffer);
+	int (*readBurst)(const struct device *dev, uint8_t addr, void *buffer, size_t size);
+	int (*writeByte)(const struct device *dev, uint8_t addr, void *buffer);
+	int (*writeBurst)(const struct device *dev, uint8_t addr, const void *buffer, size_t size);
+};
+
+struct bmp280_ioAPI
+{
+	struct bmp280_ioMethods f;
+	union bmp280_bus
+	{
+		struct i2c_dt_spec i2c;
+		struct spi_dt_spec spi;
+	} bus;
+};
+
 struct bmp280_config
 {
 	struct i2c_dt_spec i2c_bus;
@@ -75,10 +94,34 @@ struct bmp280_data
 	struct bmp280_calibData calib;
 };
 
-bool bmp280_imReady(const struct device *dev);
+/*
+ *	Helper function definitions
+ */
+
+// status getters
+static bool bmp280_isImReady(const struct device *dev);
+static bool bmp280_isMeasuring(const struct device *dev);
+static bool bmp280_chipID_OK(const struct device *dev);
+// get raw data from sensor
+static int bmp280_getPressureRaw(const struct device *dev);
+static int bmp280_getTemperatureRaw(const struct device *dev);
+
+// getters and setters of ctrl_meas
+static int bmp280_getCtrlMeas(const struct device *dev);
+static int bmp280_setCtrlMeas(const struct device *dev);
+
+// Compensation and conversion function adapted from Bosh BMP280 datasheet
+static int calibTemp(const struct device *dev, struct sensor_value *temperature);
+static int calibPress(const struct device *dev, struct sensor_value *pressure);
+
+// Waiting time estimation in force mode
+static uint16_t bmp280_timeToRead_ms(const struct device *dev);
+
+// Software reset
+static int bmp280_softReset(const struct device *dev);
 
 /*
- *	Definitions
+ *	Implementations of main API functions
  */
 
 static int bmp280_init(const struct device *dev)
@@ -92,17 +135,13 @@ static int bmp280_init(const struct device *dev)
 		printk("I2C err\n");
 		return -ENODEV;
 	}
-
-	uint8_t id;
-	int c = i2c_reg_read_byte_dt(&(conf->i2c_bus), BMP280_ADDR_ID, &id);
-	printk("chip ID = 0x%x\n", id);
-
-	if (id != BMP280_SENSOR_ID)
+	if (!bmp280_chipID_OK(dev))
 	{
 		return -ENXIO;
 	}
-	c = 0;
-	while (!bmp280_imReady(dev))
+
+	int c = 0;
+	while (!bmp280_isImReady(dev))
 	{
 		if (c > 10)
 		{
@@ -129,6 +168,7 @@ static int bmp280_init(const struct device *dev)
 	data->calib.pCalib.dP7 = (calBuf[19] << 8) | calBuf[20];
 	data->calib.pCalib.dP8 = (calBuf[21] << 8) | calBuf[22];
 	data->calib.pCalib.dP9 = (calBuf[23] << 8) | calBuf[24];
+
 	printk("%d\n", data->calib.tCalib.dT1);
 	printk("%d\n", data->calib.tCalib.dT2);
 	printk("%d\n", data->calib.tCalib.dT3);
@@ -144,53 +184,43 @@ static int bmp280_init(const struct device *dev)
 
 	data->calib.t_cooef_cmpt = 0; // reset temp calibration info readiness flag for pressure calibration
 
-	data->ctrl_meas = BMP280_OVERSAMPLING_TEMPERATURE_X_16 | BMP280_OVERSAMPLING_PRESSURE_X_16 | BMP280_MODE_NORMAL;
+	data->ctrl_meas = BMP280_CTRL_MEAS_SET_MODE(data->ctrl_meas, BMP280_MODE_FORCED);
+	data->ctrl_meas = BMP280_CTRL_MEAS_SET_OSRS_T(data->ctrl_meas, BMP280_OVERSAMPLING_TEMPERATURE_X_16);
+	data->ctrl_meas = BMP280_CTRL_MEAS_SET_OSRS_P(data->ctrl_meas, BMP280_OVERSAMPLING_PRESSURE_X_16);
+
 	c = bmp280_setCtrlMeas(dev);
 	c = bmp280_getCtrlMeas(dev);
+
 	printk("CTRL(%d) %d\n", c, (data->ctrl_meas));
 	return 0;
 }
 
-static int bmp280_getPressureRaw(const struct device *dev)
-{
-	const struct bmp280_config *conf = dev->config;
-	struct bmp280_data *data = dev->data;
-
-	int c = i2c_burst_read_dt(&(conf->i2c_bus), BMP280_ADDR_PRESS_MSB, data->press_raw, 3);
-	printk("P_c = %d", c);
-
-	if (c == 0)
-	{
-		if (data->press_raw[0] == BMP280_NO_VALUE_1)
-			return -ENODATA;
-		else
-			return 0;
-	}
-	return c;
-}
-
-static int bmp280_getTemperatureRaw(const struct device *dev)
-{
-	const struct bmp280_config *conf = dev->config;
-	struct bmp280_data *data = dev->data;
-
-	int c = i2c_burst_read_dt(&(conf->i2c_bus), BMP280_ADDR_TEMP_MSB, data->temp_raw, 3);
-	printk("T_c = %d", c);
-	if (c == 0)
-	{
-		if (data->temp_raw[0] == BMP280_NO_VALUE_1)
-			return -ENODATA;
-		else
-			return 0;
-	}
-	return c;
-}
-
 static int bmp280_sample_fetch(const struct device *dev, enum sensor_channel chanel)
 {
+	struct bmp280_data *data = dev->data;
+	if (!bmp280_chipID_OK(dev))
+	{
+		return -ENXIO;
+	}
+	if (BMP280_CTRL_MEAS_GET_MODE(data->ctrl_meas) == BMP280_MODE_FORCED || BMP280_CTRL_MEAS_GET_MODE(data->ctrl_meas) == BMP280_MODE_SLEEP)
+	{
+
+		data->ctrl_meas = BMP280_CTRL_MEAS_SET_MODE(data->ctrl_meas, BMP280_MODE_FORCED); // assuming that the aim of caling fetch in sleep mode is to triger a single measure
+		bmp280_setCtrlMeas(dev);														  // start measuring in forced mode (if in sleep wake up and force measurement)
+
+		k_msleep(bmp280_timeToRead_ms(dev)); // wait for result based on configuration
+		printk("wait %dms\n", bmp280_timeToRead_ms(dev));
+		uint8_t cycles = 0;
+		while (bmp280_isMeasuring(dev) || !bmp280_isImReady(dev))
+		{
+			if (cycles >= 500)
+				return -ETIMEDOUT;
+			k_usleep(100);
+		}
+	}
 	int c = bmp280_getCtrlMeas(dev);
 	printk("CTRL(%d) %d\n", c, (((struct bmp280_data *)dev->data)->ctrl_meas));
-	((struct bmp280_data *)dev->data)->calib.t_cooef_cmpt = 0;
+	data->calib.t_cooef_cmpt = 0;
 	switch (chanel)
 	{
 	case SENSOR_CHAN_AMBIENT_TEMP:
@@ -214,8 +244,85 @@ static int bmp280_sample_fetch(const struct device *dev, enum sensor_channel cha
 	return 0;
 }
 
+static int bmp280_chanel_get(const struct device *dev, enum sensor_channel chanel, struct sensor_value *reading)
+{
+	switch (chanel)
+	{
+	case SENSOR_CHAN_AMBIENT_TEMP:
+		printk("temp raw = %d\n", ((struct bmp280_data *)(dev->data))->temp_raw[0]);
+		calibTemp(dev, reading);
+		break;
+	case SENSOR_CHAN_PRESS:
+		if (!((struct bmp280_data *)(dev->data))->calib.t_cooef_cmpt)
+		{
+			struct sensor_value dummy;
+			calibTemp(dev, &dummy);
+		}
+		calibPress(dev, reading);
+
+		break;
+	default:
+		return -ENOTSUP;
+	}
+	return 0;
+}
+
+/*
+ *	Implementations of helper functions
+ */
+
+static int bmp280_setCtrlMeas(const struct device *dev)
+{
+	const struct bmp280_config *conf = dev->config;
+	const struct bmp280_data *data = dev->data;
+	return i2c_reg_write_byte_dt(&(conf->i2c_bus), BMP280_ADDR_CTRL_MEAS, data->ctrl_meas);
+}
+
+static int bmp280_getCtrlMeas(const struct device *dev)
+{
+	const struct bmp280_config *conf = dev->config;
+	struct bmp280_data *data = dev->data;
+	return i2c_reg_read_byte_dt(&(conf->i2c_bus), BMP280_ADDR_CTRL_MEAS, &data->ctrl_meas);
+}
+
+static int bmp280_getPressureRaw(const struct device *dev)
+{
+
+	const struct bmp280_config *conf = dev->config;
+	struct bmp280_data *data = dev->data;
+
+	int c = i2c_burst_read_dt(&(conf->i2c_bus), BMP280_ADDR_PRESS_MSB, data->press_raw, 3);
+	printk("P_c = %d\n", c);
+
+	if (c == 0)
+	{
+		if (data->press_raw[0] == BMP280_NO_VALUE_1 && BMP280_CTRL_MEAS_GET_OSRS_P(data->ctrl_meas) != BMP280_OVERSAMPLING_PRESSURE_X_0)
+			return -ENODATA;
+		else
+			return 0;
+	}
+	return c;
+}
+
+static int bmp280_getTemperatureRaw(const struct device *dev)
+{
+	const struct bmp280_config *conf = dev->config;
+	struct bmp280_data *data = dev->data;
+
+	int c = i2c_burst_read_dt(&(conf->i2c_bus), BMP280_ADDR_TEMP_MSB, data->temp_raw, 3);
+	printk("T_c = %d\n", c);
+	if (c == 0)
+	{
+		if (data->temp_raw[0] == BMP280_NO_VALUE_1 && BMP280_CTRL_MEAS_GET_OSRS_T(data->ctrl_meas) != BMP280_OVERSAMPLING_TEMPERATURE_X_0)
+			return -ENODATA;
+		else
+			return 0;
+	}
+	return c;
+}
+
 // Adapted from Bosh BMP280 datasheet
-static uint8_t calibTemp(const struct device *dev, struct sensor_value *temperature)
+static int calibTemp(const struct device *dev, struct sensor_value *temperature)
 {
 	struct bmp280_data *data = dev->data;
 
@@ -245,7 +352,7 @@ static uint8_t calibTemp(const struct device *dev, struct sensor_value *temperat
 }
 
 // Adapted from Bosh BMP280 datasheet
-static uint8_t calibPress(const struct device *dev, struct sensor_value *pressure)
+static int calibPress(const struct device *dev, struct sensor_value *pressure)
 {
 	printk("press calib\n");
 
@@ -272,57 +379,104 @@ static uint8_t calibPress(const struct device *dev, struct sensor_value *pressur
 	return 0;
 }
 
-static int bmp280_chanel_get(const struct device *dev, enum sensor_channel chanel, struct sensor_value *reading)
-{
-	switch (chanel)
-	{
-	case SENSOR_CHAN_AMBIENT_TEMP:
-		printk("temp raw = %d\n", ((struct bmp280_data *)(dev->data))->temp_raw[0]);
-		calibTemp(dev, reading);
-		break;
-	case SENSOR_CHAN_PRESS:
-		if (!((struct bmp280_data *)(dev->data))->calib.t_cooef_cmpt)
-		{
-			struct sensor_value dummy;
-			calibTemp(dev, &dummy);
-		}
-		calibPress(dev, reading);
-
-		break;
-	default:
-		return -ENOTSUP;
-	}
-	return 0;
-}
-
-int bmp280_setCtrlMeas(const struct device *dev)
-{
-	const struct bmp280_config *conf = dev->config;
-	const struct bmp280_data *data = dev->data;
-	return i2c_reg_write_byte_dt(&(conf->i2c_bus), BMP280_ADDR_CTRL_MEAS, data->ctrl_meas);
-}
-
-int bmp280_getCtrlMeas(const struct device *dev)
-{
-	const struct bmp280_config *conf = dev->config;
-	struct bmp280_data *data = dev->data;
-	return i2c_reg_read_byte_dt(&(conf->i2c_bus), BMP280_ADDR_CTRL_MEAS, &data->ctrl_meas);
-}
-
-
-bool bmp280_imReady(const struct device *dev)
+static bool bmp280_isImReady(const struct device *dev)
 {
 	const struct bmp280_config *config = dev->config;
 	uint8_t status = 0;
 	i2c_reg_read_byte_dt(&(config->i2c_bus), BMP280_ADDR_STATUS, &status);
-	return !(status & 0b00000001);
+	return !(status & BMP280_MASK_STATUS_IM_UPADTE);
 }
 
+static bool bmp280_isMeasuring(const struct device *dev)
+{
+	const struct bmp280_config *config = dev->config;
+	uint8_t status = 0;
+	i2c_reg_read_byte_dt(&(config->i2c_bus), BMP280_ADDR_STATUS, &status);
+	return (status & BMP280_MASK_STATUS_MEASURING) >> 3;
+}
+static bool bmp280_chipID_OK(const struct device *dev)
+{
+	uint8_t id;
+	const struct bmp280_config *conf = dev->config;
+	int c = i2c_reg_read_byte_dt(&(conf->i2c_bus), BMP280_ADDR_ID, &id);
+	if (id != BMP280_SENSOR_ID || c)
+	{
+		return 0;
+	}
+	return 1;
+}
+static int bmp280_softReset(const struct device *dev)
+{
+	const struct bmp280_config *conf = dev->config;
 
+	if (!device_is_ready(conf->i2c_bus.bus))
+	{
+		printk("I2C err\n");
+		return -ENODEV;
+	}
+	int c = i2c_reg_write_byte_dt(&(conf->i2c_bus), BMP280_ADDR_RESET, BMP280_RESET);
+	if (c)
+		return c;
+	return bmp280_init(dev);
+}
+
+static uint16_t bmp280_timeToRead_ms(const struct device *dev)
+{
+	struct bmp280_data *data = dev->data;
+	uint16_t t = 0;
+	switch (BMP280_CTRL_MEAS_GET_OSRS_T(data->ctrl_meas))
+	{
+	case BMP280_OVERSAMPLING_TEMPERATURE_X_0:
+		t += 0;
+		break;
+	case BMP280_OVERSAMPLING_TEMPERATURE_X_1:
+		t += 2;
+		break;
+	case BMP280_OVERSAMPLING_TEMPERATURE_X_2:
+		t += 4;
+		break;
+	case BMP280_OVERSAMPLING_TEMPERATURE_X_4:
+		t += 8;
+		break;
+	case BMP280_OVERSAMPLING_TEMPERATURE_X_8:
+		t += 16;
+		break;
+	case BMP280_OVERSAMPLING_TEMPERATURE_X_16:
+		t += 32;
+		break;
+	default:
+		break;
+	}
+	switch (BMP280_CTRL_MEAS_GET_OSRS_P(data->ctrl_meas))
+	{
+	case BMP280_OVERSAMPLING_PRESSURE_X_0:
+		t += 0;
+		break;
+	case BMP280_OVERSAMPLING_PRESSURE_X_1:
+		t += 2;
+		break;
+	case BMP280_OVERSAMPLING_PRESSURE_X_2:
+		t += 4;
+		break;
+	case BMP280_OVERSAMPLING_PRESSURE_X_4:
+		t += 8;
+		break;
+	case BMP280_OVERSAMPLING_PRESSURE_X_8:
+		t += 16;
+		break;
+	case BMP280_OVERSAMPLING_PRESSURE_X_16:
+		t += 32;
+		break;
+	default:
+		break;
+	}
+	t += 2; // measurment starting time
+	return t;
+}
 
 /*
-*	Zephyr sensor API integration
-*/
+ *	Zephyr sensor API integration
+ */
 
 const struct sensor_driver_api bmp280_api = {
 	.attr_set = NULL,
